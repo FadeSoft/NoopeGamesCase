172d2ff2a5362aa419f97cabd7accb07629ef938
Samed Batman <samet25batman@gmail.com>
2022-11-01 15:07:27 +0300

    Feat: Money Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(1, 10)]
+    public int widht, height;
+    Vector3 origin = Vector3.zero;
+
+    public GameObject[] collectables;
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < widht; x++)
+            {
+                int randomValue = Random.Range(0, 3);
+                GameObject obj = Instantiate(collectables[0], origin, transform.rotation, transform);
+                origin = new Vector3((x + 1)*1.5f, 0f, z);
+            }
+            origin = new Vector3(0f, 0f, z + 1);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private float Center(float value)
     {
-        
+        return (value - 1f) / 2f;
     }
 }
