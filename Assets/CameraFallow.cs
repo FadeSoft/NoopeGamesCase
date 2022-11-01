d3b3446cb4fdb5579f976c1332e97f7ba36a605e
Samed Batman <samet25batman@gmail.com>
2022-11-01 23:06:04 +0300

    Feat: Money Texture

--- Assets/CameraFallow.cs
+++ Assets/CameraFallow.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFallow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-  
-
     void Update()
     {
         transform.position = target.position + offset;
